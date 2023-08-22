332cab1a7b8a020af6767011a8f6896259f014d8
Vinícius Tavares Mota <106537834+viniciustmota@users.noreply.github.com>
2023-08-22 16:17:25 -0300

    Finalização do software

---
+++ vogal-consoante/vogal-consoante/numero por extenso/numero por extenso/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace numero_por_extenso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboLetras = new System.Windows.Forms.ComboBox();
+            this.txtResultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SELECIONE UM NÚMERO:";
+            // 
+            // cboLetras
+            // 
+            this.cboLetras.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboLetras.FormattingEnabled = true;
+            this.cboLetras.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.cboLetras.Location = new System.Drawing.Point(360, 45);
+            this.cboLetras.Name = "cboLetras";
+            this.cboLetras.Size = new System.Drawing.Size(65, 32);
+            this.cboLetras.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.AutoSize = true;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(122, 231);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(0, 24);
+            this.txtResultado.TabIndex = 2;
+            this.txtResultado.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LIMPAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(361, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "FECHAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(126, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 61);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "CHECAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.cboLetras);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboLetras;
+        private System.Windows.Forms.Label txtResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ vogal-consoante/vogal-consoante/numero por extenso/numero por extenso/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numero_por_extenso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DialogResult mensagem = MessageBox.Show("Deseja finalizar a aplicação?", "Encerrar", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2); if (mensagem == DialogResult.Yes) { this.Close(); }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            switch (cboLetras.Text)
+            {
+                case "A":
+                    txtResultado.Text = "Vogal";
+                    break;
+                case "B":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "C":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "D":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "E":
+                    txtResultado.Text = "Vogal";
+                    break;
+                case "F":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "G":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "H":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "I":
+                    txtResultado.Text = "Vogal";
+                    break;
+                case "J":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "K":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "L":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "M":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "N":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "O":
+                    txtResultado.Text = "Vogal";
+                    break;
+                case "P":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "Q":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "R":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "S":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "T":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "U":
+                    txtResultado.Text = "Vogal";
+                    break;
+                case "V":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "W":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "X":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "Y":
+                    txtResultado.Text = "Consoante";
+                    break;
+                case "Z":
+                    txtResultado.Text = "Consoante";
+                    break;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cboLetras.Text = "";
+            txtResultado.Text = "";
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
